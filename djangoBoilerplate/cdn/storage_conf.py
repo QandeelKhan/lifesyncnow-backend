0d228fb43197118c5ae5a4fd6e7c6296ea137c9b
QandeelKhan <com.qandeelhaider@gmail.com>
2023-02-03 23:53:20 +0500

    initial commit

--- djangoBoilerplate/cdn/storage_conf.py
+++ djangoBoilerplate/cdn/storage_conf.py
@@ -14,22 +14,22 @@ if USE_SPACES:
     AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # static settings
-    AWS_LOCATION = 'space-StorefrontBackend2/static'
+    AWS_LOCATION = 'space-django-boilerplate/static'
     STATIC_URL = f'https://shopingly-space.fra1.digitaloceanspaces.com/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # public media settings
-    PUBLIC_MEDIA_LOCATION = 'space-StorefrontBackend2/media'
+    PUBLIC_MEDIA_LOCATION = 'space-django-boilerplate/media'
     MEDIA_URL = f'https://shopingly-space.fra1.digitaloceanspaces.com/{PUBLIC_MEDIA_LOCATION}/'
 
-    DEFAULT_FILE_STORAGE = 'shoppingly.cdn.backends.MediaRootS3Boto3Storage'
+    DEFAULT_FILE_STORAGE = '<space-name>.cdn.backends.MediaRootS3Boto3Storage'
 else:
     STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / "space-StorefrontBackend2/static"
+    STATIC_ROOT = BASE_DIR / "space-django-boilerplate/static"
     MEDIA_URL = '/media/'
-    MEDIA_ROOT = BASE_DIR / 'space-StorefrontBackend2/media'
+    MEDIA_ROOT = BASE_DIR / 'space-django-boilerplate/media'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'space-StorefrontBackend2/static'),
+    os.path.join(BASE_DIR, 'space-django-boilerplate/static'),
 )
 # helping material
 # https://testdriven.io/blog/django-digitalocean-spaces/
--- initialize.py
+++ initialize.py
@@ -1,3 +1,6 @@
+import os
+import re
+import subprocess
 import psycopg2
 
 
@@ -27,3 +30,57 @@ if __name__ == "__main__":
     dbname = input("Enter the database name: ")
     create_user_and_database(username, password, dbname)
     print("User and database created successfully.")
+
+# -----REPLACING INSTANCES
+
+# Define the word you want to replace and the word you want to replace it with
+word = input("Enter the word: ")
+replace_with = input("Enter the new word to replace with old one: ")
+
+# Search through all the Python files in your Django app
+for root, dir, files in os.walk("/path/to/your/django/app"):
+    for file in files:
+        if file.endswith(".py"):
+            file_path = os.path.join(root, file)
+            with open(file_path, "r") as f:
+                content = f.read()
+
+            # Replace all instances of the old word with the new word
+            content = re.sub(word, replace_with, content)
+
+            with open(file_path, "w") as f:
+                f.write(content)
+
+# ------DELETING OLD VIRTUAL ENVIRONMENT AND CREATING NEW_ONE
+
+# Get the root directory of the current project
+root_dir = os.getcwd()
+
+# Prompt the user to confirm if they want to create a new virtual environment
+create_new_venv = input(
+    "Do you want to create a new virtual environment for this project? (y/n): ")
+if create_new_venv.lower() not in ["yes", "y"]:
+    print("Skipping the creation of a new virtual environment.")
+else:
+    # Prompt the user for the name of the new virtual environment
+    new_venv_name = input(
+        "Enter the name of the new virtual environment (without 'venv-' prefix): ")
+    venv_name = "venv-" + new_venv_name
+
+    # Delete the old virtual environment folder
+    venv_path = os.path.join(root_dir, venv_name)
+    if os.path.exists(venv_path):
+        print(f"Deleting the old virtual environment: {venv_path}")
+        subprocess.call(["rm", "-rf", venv_path])
+
+    # Create a new virtual environment
+    print(f"Creating a new virtual environment: {venv_path}")
+    subprocess.call(["python3", "-m", "venv", venv_path])
+
+    # Activate the virtual environment
+    activate_script = os.path.join(venv_path, "bin", "activate")
+    subprocess.call(["source", activate_script])
+
+    # Install the dependencies from the requirements.txt file
+    requirements_file = os.path.join(root_dir, "requirements.txt")
+    subprocess.call(["pip", "install", "-r", requirements_file])
