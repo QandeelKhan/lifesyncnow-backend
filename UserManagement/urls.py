dc1f3502a5d96635959dbce4da83cb52d6c133cd
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-07 19:13:51 +0500

    auto commit by py script

--- UserManagement/urls.py
+++ UserManagement/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.UserRegistrationView.as_view(), name='register'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('user/register/', views.UserRegistrationView.as_view(), name='register'),
+    path('user/login/', views.UserLoginView.as_view(), name='login'),
     path('profile/', views.UserProfileView.as_view(), name='profile'),
     path('changepassword/', views.UserChangePasswordView.as_view(),
          name='changepassword'),
