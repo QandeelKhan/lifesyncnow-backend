15985ff9bfa6deabad626e6b58a59636a5cc84c0
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-03-11 04:35:52 +0500

    auto commit by py script

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView, CommentCreateView, ReplyCreateView
 
 urlpatterns = [
-    path('', BlogPostListView.as_view(), name='blog-list'),
+    path('blog/posts-list', BlogPostListView.as_view(), name='blog-list'),
     path('<pk>/', BlogPostDetailView.as_view(), name='blog-detail'),
     path('create/', BlogPostCreateView.as_view(), name='blog-create'),
     path('<pk>/update/', BlogPostUpdateView.as_view(), name='blog-update'),
