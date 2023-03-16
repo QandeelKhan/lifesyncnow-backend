185f312395c976b5c9580550920338a6ee74da53
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-03-17 02:07:26 +0500

    auto commit by py script

---
+++ run-migrations.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# echo "Waiting for database to start..."
+
+# Wait for the database to start before running migrations
+# until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+#     echo >&2 "Postgres is unavailable - sleeping"
+#     sleep 1
+# done
+
+# echo "Postgres is up - running migrations..."
+
+# Run migrations
+python manage.py makemigrations UserManagement
+python manage.py migrate UserManagement
+python manage.py makemigrations blog
+python manage.py migrate blog
+python manage.py migrate
+echo "Migrations complete"
+echo "running our django server"
+python manage.py runserver 0.0.0.0:8000
