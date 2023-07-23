0c73a65c11cb5c63bc5802dbc33de281951a984d
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-07-23 17:28:26 +0500

    auto commit by py script

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView, CommentCreateView, ReplyCreateView, BlogPostByCategoryView, TopicListView, BlogPostListByCategoryAndTopicAPIView, enqueue_load_posts
 
 urlpatterns = [
-    path('blog/posts-list', BlogPostListView.as_view(), name='blog-list'),
-    path('blog/enqueue_load_posts', enqueue_load_posts, name='enqueue-list'),
+    path('blog/posts-list/', BlogPostListView.as_view(), name='blog-list'),
     path('blog/category/<str:category_slug>/',
          BlogPostByCategoryView.as_view(), name='blog_post_by_category'),
     path('blog/category/<str:category_slug>/<str:topic_slug>/',
@@ -15,4 +14,5 @@ urlpatterns = [
     path('<pk>/comments/', CommentCreateView.as_view(), name='post-comments'),
     path('<pk>/comments/<comment_id>/reply/',
          ReplyCreateView.as_view(), name='comment-reply'),
+    path('blog/enqueue_load_posts', enqueue_load_posts, name='enqueue-list'),
 ]
